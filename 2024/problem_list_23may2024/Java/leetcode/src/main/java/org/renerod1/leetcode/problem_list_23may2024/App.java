358a7012424989527b3626c8318681b244cb2126
Rene Rodriguez <renerod1@hotmail.com>
2024-05-24 09:19:13 -0400

    LeetCode question in weekly email 23-MAY-2024

---
+++ 2024/problem_list_23may2024/Java/leetcode/src/main/java/org/renerod1/leetcode/problem_list_23may2024/App.java
@@ -0,0 +1,18 @@
+package org.renerod1.leetcode.problem_list_23may2024;
+
+class App {
+    public static void main(String[] args) {
+        App s = new App();
+        s.isMatch("aa", "a");
+        s.isMatch("aa", "a*");
+        s.isMatch("ab", ".*");
+        s.isMatch("abc", "a***abc");
+    }
+
+    public boolean isMatch(String s, String p) {
+        while (p.contains("**")) {
+            p = p.replace("**", "*");
+        }
+        return s.matches(p);
+    }
+}
---
+++ 2024/problem_list_23may2024/Java/leetcode/src/test/java/org/renerod1/leetcode/problem_list_23may2024/AppTest.java
@@ -0,0 +1,30 @@
+package org.renerod1.leetcode.problem_list_23may2024;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    App s = new App();
+
+    @Test
+    public void case1() {
+        assertEquals(false, s.isMatch("aa", "a"));
+    }
+
+    @Test
+    public void case2() {
+        assertEquals(true, s.isMatch("aa", "a*"));
+    }
+
+    @Test
+    public void case3() {
+        assertEquals(true, s.isMatch("ab", ".*"));
+    }
+
+    @Test
+    public void case4() {
+        assertEquals(true, s.isMatch("abc", "a***abc"));
+    }
+}
