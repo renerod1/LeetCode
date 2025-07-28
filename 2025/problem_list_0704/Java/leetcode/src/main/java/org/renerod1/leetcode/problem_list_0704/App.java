417f13587a5a3b332ad2424f9a88ab417cba6987
Rene Rodriguez <renerod1@hotmail.com>
2025-07-28 02:38:40 -0400

    Actually did a binary search to solve the problem

--- 2025/problem_list_0704/Java/leetcode/src/main/java/org/renerod1/leetcode/problem_list_0704/App.java
+++ 2025/problem_list_0704/Java/leetcode/src/main/java/org/renerod1/leetcode/problem_list_0704/App.java
@@ -1,8 +1,5 @@
 package org.renerod1.leetcode.problem_list_0704;
 
-import java.util.Arrays;
-import java.util.List;
-
 class App {
     public static void main(String[] args) {
         App app = new App();
@@ -11,8 +8,22 @@ class App {
     }
 
     public int search(int[] nums, int target) {
-        List<Integer> a = Arrays.stream(nums).boxed().toList();
+        int left = 0;
+        int right = nums.length - 1;
 
-        return a.indexOf(target);
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+            else if(nums[mid] > target) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return -1;
     }
 }
